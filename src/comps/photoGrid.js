ecd3d7d01fc80e7e04248ee06f435ab29813ec14
Aaryan Dewan <aaryandewan@yahoo.com>
2021-05-11 12:46:34 +0530

    final commit

--- src/comps/photoGrid.js
+++ src/comps/photoGrid.js
@@ -2,23 +2,16 @@ import React from "react";
 import firebase from "firebase";
 import { useState, useEffect } from "react";
 import { TempCustomHook } from "../context";
+import { Container, Row, Col } from "react-bootstrap";
 
 const PhotoGrid = () => {
   const { counter, setCounter } = TempCustomHook();
+  const [tempCounter, setTempCounter] = useState(0);
   let db = firebase.firestore();
   const [photoArr, setPhotoArr] = useState([]);
   const allPhotos = [];
 
   useEffect(() => {
-    // db.collection("users")
-    //   .get()
-    //   .then((querySnapshot) => {
-    //     querySnapshot.forEach((doc) => {
-    //       setPhotoArr((oldArr) => [...oldArr, doc.data().avatar]);
-    //       console.log("ZZZ", doc.data());
-    //     });
-    //   });
-
     db.collection("users").onSnapshot((querySnapshot) => {
       console.log("Fired");
       querySnapshot.forEach((doc) => {
@@ -27,40 +20,23 @@ const PhotoGrid = () => {
       });
     });
 
-    // db.collection("users")
-
-    //   .get()
-    //   .then((querySnapshot) => {
-    //     querySnapshot.forEach((doc) => {
-    //       setPhotoArr((oldArr) => [...oldArr, doc.data().avatar]);
-    //       console.log("ZZZ", doc.data());
-    //     });
-    //   });
-
     setPhotoArr(allPhotos);
     console.log(photoArr);
   }, [counter]);
 
   return (
     <>
-      <div
-        style={{
-          display: "grid",
-          gridGap: "50px 100px",
-          gridTemplateColumns: "1fr 1fr 1fr",
-          gridGap: "1rem",
-        }}
-      >
-        {photoArr.map((image) => {
-          return (
-            <img
-              src={image}
-              style={{ height: "40vh", width: "20vw", paddingRight: "20px" }}
-            ></img>
-          );
-        })}
-        {console.log("don is ", photoArr)}
-      </div>
+      <Container>
+        <Row className="row-cols-1 row-cols-sm-2 row-cols-md-4">
+          {photoArr.map((image) => {
+            return (
+              <Col className="mb-4 mr-4">
+                <img src={image} className="img-fluid"></img>
+              </Col>
+            );
+          })}
+        </Row>
+      </Container>
     </>
   );
 };
